6c5babfc410587203dba2af2976055a5d4557da6
Naveen Adhithya <naveenadhithya123@gmail.com>
2026-03-26 15:10:30 +0530

    Switch backend mail transport to Brevo SMTP

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,10 +1,12 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
-    user: "lap100gbfree@gmail.com",
-    pass: "ehjh nmfh odsb shco"
+    user: "a5bbdb001@smtp-brevo.com",
+    pass: "xsmtpsib-62603cbb9a280ce7ba48cd056c53b027c2f48a58f8e518a471d778854d5b127f-KvhR3ta1KafYJmZO"
   }
 });
 
