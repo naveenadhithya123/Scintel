cd886fc773245fc3f49e3a91591e805c9164b575
Naveen Adhithya <naveenadhithya123@gmail.com>
2026-03-26 15:25:45 +0530

    Rotate Brevo SMTP key

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
   secure: false,
   auth: {
     user: "a5bbdb001@smtp-brevo.com",
-    pass: "xsmtpsib-62603cbb9a280ce7ba48cd056c53b027c2f48a58f8e518a471d778854d5b127f-KvhR3ta1KafYJmZO"
+    pass: "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"
   }
 });
 
